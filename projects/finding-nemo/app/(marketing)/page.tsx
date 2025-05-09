516d2bd1627995453fe4555abdb4875065abd64e
gayashan4lk <gayashan4lk@gmail.com>
2025-05-10 00:23:26 +0530

    refactor: clean up landing page structure; remove unused components and improve content presentation

--- projects/finding-nemo/app/(marketing)/page.tsx
+++ projects/finding-nemo/app/(marketing)/page.tsx
@@ -2,28 +2,26 @@ import Link from 'next/link'
 
 export default function Page() {
 	return (
-		<>
-			<div>Landing page</div>
-			{/* <div>
-				<div className="w-full flex flex-row gap-4">
-					<Link href={'/dashboard'} className="text-blue-600 hover:underline">
-						Dashboard
-					</Link>
-					<Link href={'/issues'} className="text-blue-600 hover:underline">
-						Issues
-					</Link>
-					<Link href={'/about-us'} className="text-blue-600 hover:underline">
-						About Us
-					</Link>
-					<Link href={'/contact-us'} className="text-blue-600 hover:underline">
-						Contact Us
+		<div className="max-w-7xl mx-auto px-4 sm:px-6 py-16 sm:py-24">
+			<div className="text-center">
+				<h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-800 tracking-tight">
+					Issue tracking <br className="hidden sm:block" />
+					<span className="text-purple-600 dark:text-purple-500">
+						simplified
+					</span>
+				</h1>
+				<p className="mt-6 max-w-2xl mx-auto text-lg sm:text-xl text-gray-500">
+					A minimal and elegant issue tracking tool for modern teams. Manage
+					your projects with ease.
+				</p>
+				<div className="mt-10">
+					<Link
+						href="/signup"
+						className="font-medium p-4 bg-purple-500 text-white rounded-3xl hover:bg-purple-600">
+						Get Started
 					</Link>
 				</div>
-				<div className="flex flex-col items-center justify-center h-screen">
-					<h1 className="text-xl">Wecome to project</h1>
-					<h1 className="text-7xl font-bold">Finding Nemo</h1>
-				</div>
-			</div> */}
-		</>
+			</div>
+		</div>
 	)
 }
