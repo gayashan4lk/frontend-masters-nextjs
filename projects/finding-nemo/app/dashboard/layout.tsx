e2b8199339e0d0806ba316511b5ab70b3ea2b7ba
gayashan4lk <gayashan4lk@gmail.com>
2025-06-01 18:30:44 +0530

    chore: update next.js version from 15.3.1 to 15.3.3 in package.json

--- projects/finding-nemo/app/dashboard/layout.tsx
+++ projects/finding-nemo/app/dashboard/layout.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from 'react'
 import DashboardSkeleton from '../components/DashboardSkeleton'
 import Navigation from '../components/Navigation'
+import Link from 'next/link'
+import Button from '../components/ui/Button'
+import { PlusIcon } from 'lucide-react'
 
 export default async function DashboardLayout({
 	children,
@@ -11,7 +14,16 @@ export default async function DashboardLayout({
 		<div className="flex min-h-screen">
 			<Navigation />
 			<main className="mx-8">
+				<h1 className="text-2xl font-bold">Issues</h1>
+				<Link href="/issues/new">
+					<Button>
+						<span className="flex items-center">
+							<PlusIcon size={18} className="mr-2" /> New Issues
+						</span>
+					</Button>
+				</Link>
 				<Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+				{/* {children} */}
 			</main>
 		</div>
 	)
--- projects/finding-nemo/app/dashboard/page.tsx
+++ projects/finding-nemo/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { getCurrentUser, getIssuesByUserId } from '@/lib/data'
 import Link from 'next/link'
 import Button from '@/app/components/ui/Button'
 import { PlusIcon } from 'lucide-react'
+import { Suspense } from 'react'
 
 export default async function Page() {
 	// try {
@@ -16,51 +17,46 @@ export default async function Page() {
 
 	return (
 		<div>
-			{user && (
+			{/* {user && (
 				<div>
 					<h2>{user.email}</h2>
 				</div>
-			)}
-			<h1 className="text-2xl font-bold">Issues</h1>
-			<Link href="/issues/new">
-				<Button>
-					<span className="flex items-center">
-						<PlusIcon size={18} className="mr-2" /> New Issues
-					</span>
-				</Button>
-			</Link>
-			{issues && issues.length > 0 ? (
-				<div className="mt-6">
-					<ul className="space-y-4">
-						{issues.map(issue => (
-							<li
-								key={issue.id}
-								className="p-4 border border-gray-200 rounded-lg">
-								<h3 className="text-lg font-semibold">{issue.title}</h3>
-								<p className="text-gray-600">{issue.description}</p>
-								<p className="text-sm text-gray-500">
-									Created at: {new Date(issue.createdAt).toLocaleDateString()}
-								</p>
-							</li>
-						))}
-					</ul>
-				</div>
-			) : (
-				<div className="flex flex-col items-center justify-center py-12 text-center border border-gray-200 rounded-lg bg-white p-8">
-					<h3 className="text-lg font-medium mb-2">No issues found</h3>
-					<p className="text-gray-500 mb-6">
-						Get started by creating your first issue.
-					</p>
-					<Link href="/issues/new">
-						<Button>
-							<span className="flex items-center">
-								<PlusIcon size={18} className="mr-2" />
-								Create Issue
-							</span>
-						</Button>
-					</Link>
-				</div>
-			)}
+			)} */}
+
+			<Suspense fallback={<span>Loading issues...</span>}>
+				{issues && issues.length > 0 ? (
+					<div className="mt-6">
+						<ul className="space-y-4">
+							{issues.map(issue => (
+								<li
+									key={issue.id}
+									className="p-4 border border-gray-200 rounded-lg">
+									<h3 className="text-lg font-semibold">{issue.title}</h3>
+									<p className="text-gray-600">{issue.description}</p>
+									<p className="text-sm text-gray-500">
+										Created at: {new Date(issue.createdAt).toLocaleDateString()}
+									</p>
+								</li>
+							))}
+						</ul>
+					</div>
+				) : (
+					<div className="flex flex-col items-center justify-center py-12 text-center border border-gray-200 rounded-lg bg-white p-8">
+						<h3 className="text-lg font-medium mb-2">No issues found</h3>
+						<p className="text-gray-500 mb-6">
+							Get started by creating your first issue.
+						</p>
+						<Link href="/issues/new">
+							<Button>
+								<span className="flex items-center">
+									<PlusIcon size={18} className="mr-2" />
+									Create Issue
+								</span>
+							</Button>
+						</Link>
+					</div>
+				)}
+			</Suspense>
 		</div>
 	)
 }
